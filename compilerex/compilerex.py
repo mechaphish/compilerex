897d47ee1f1aadd371000b4c9429c85597e5b267
Chris Salls <salls@cs.ucsb.edu>
2016-03-08 21:21:47 -0800

    add compile_from_string

--- compilerex/compilerex.py
+++ compilerex/compilerex.py
@@ -1,6 +1,9 @@
 import subprocess
+import tempfile
 import os
 
+class CompileError(Exception):
+    pass
 
 def compile(args):
     if not isinstance(args, list):
@@ -9,3 +12,21 @@ def compile(args):
     p = subprocess.Popen(["bash", path] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res = p.communicate()
     return res
+
+def compile_from_string(c_str, filename=None):
+    c_file = tempfile.mktemp(dir='/tmp/', prefix="c_file_")
+
+    with open(c_file, "wb") as f:
+        f.write(c_str)
+
+    if filename is None:
+        filename = tempfile.mktemp(dir='/tmp/', prefix="compiled_")
+    res = compile([c_file, "-o", filename])
+
+    if res[1] != "":
+        raise CompileError(res[1])
+
+    with open(filename, "rb") as f:
+        result = f.read()
+
+    return result
