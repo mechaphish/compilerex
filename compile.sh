11073517eaab37415d56ed42789ae17b98225979
Nick Stephens <nick.d.stephens@gmail.com>
2016-06-17 01:17:41 -0700

    Code with warnings is acceptable

--- compile.sh
+++ compile.sh
@@ -23,7 +23,7 @@ export PATH="$DIR/bin:$PATH"
 
 function compile() {
     $CC -Ilib/ \
-        -std=gnu99 -Wall -Wextra -Wshadow -Wwrite-strings -Wpointer-arith -Wstrict-overflow=4  \
+        -std=gnu99 -Wextra -Wshadow -Wwrite-strings -Wpointer-arith -Wstrict-overflow=4  \
         $CFLAGS $LDFLAGS $@ $LDLIBS;
 }
 
