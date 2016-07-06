13320322e0e916068445edf9058948dd76d7ff9c
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-06 02:36:02 -0700

    Check returncode instead of stderr for CompileError

--- compilerex/compilerex.py
+++ compilerex/compilerex.py
@@ -11,7 +11,8 @@ def compile(args):
     path = os.path.join(os.path.dirname(__file__), "..", "compile.sh")
     p = subprocess.Popen(["bash", path] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res = p.communicate()
-    return res
+    returncode = p.wait()
+    return returncode, res
 
 def assemble(args):
     if not isinstance(args, list):
@@ -19,7 +20,8 @@ def assemble(args):
     path = os.path.join(os.path.dirname(__file__), "..", "assemble.sh")
     p = subprocess.Popen(["bash", path] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     res = p.communicate()
-    return p.returncode, res
+    returncode = p.wait()
+    return returncode, res
 
 def compile_from_string(c_str, filename=None):
     '''
@@ -34,9 +36,9 @@ def compile_from_string(c_str, filename=None):
     outfile = filename
     if filename is None:
         outfile = tempfile.mktemp(dir='/tmp/', prefix="compiled_")
-    res = compile([c_file, "-o", outfile])
+    retcode, res = compile([c_file, "-o", outfile])
 
-    if res[1] != "":
+    if retcode != 0:
         raise CompileError(res[1])
 
     with open(outfile, "rb") as f:
