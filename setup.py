0496543217651af305f7baf8aa91747abcd8e84a
Chris Salls <salls@cs.ucsb.edu>
2016-07-17 02:46:52 -0700

    ensure executable

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
+from distutils.command.build import build as _build
+import os
+
+BIN_PATH = "bin"
+
+
+class build(_build):
+    def run(self):
+        for f in os.listdir(BIN_PATH):
+            os.chmod(os.path.join("bin", f), 0777)
+
 
 setup(
-      name='compilerex',
-      version='0.1',
-      packages=['compilerex'],
-      install_requires=[
-      ],
+    name='compilerex',
+    version='0.1',
+    packages=['compilerex'],
+    install_requires=[
+    ],
+    cmdclass={'build': build}
 )
-
