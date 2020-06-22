af46300bdd0fa83b730f0e21c62b08865b230c9e
Fish <ltfish@users.noreply.github.com>
2020-06-21 17:49:31 -0700

    Detecting compilers automatically (#3)
    * Auto detect compiler and args with `auto_assemble`
    
    * Added c_to_asm to create reassembler-friendly asm from C
    
    * Fork branch (#2)
    
    * Fix import
    
    * Py3 str<->bytes fixes
    
    * Disable stack protector when compiling
    
    * Disable stack-protector and pie globally for gcc
    
    * Omit byte decoding by opening file in the right mode
    
    * Use python3.8 compatible octal integer
    
    Co-authored-by: Paul Grosen <pcgrosen@gmail.com>
    Co-authored-by: Moritz Eckert <m1gh7ym0@gmail.com>

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ from distutils.core import setup
 from distutils.command.build import build as _build
 import os
 
+
 BIN_PATH = "bin"
+
 
 class build(_build):
     def run(self):
         for f in os.listdir(BIN_PATH):
             os.chmod(os.path.join("bin", f), 0o777)
 
+
 setup(
     name='compilerex',
     version='0.1',
     packages=['compilerex'],
     install_requires=[
+        'python-magic'
     ],
     cmdclass={'build': build}
 )
